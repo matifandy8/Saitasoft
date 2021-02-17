707ae2ce3eeede79e76a887a077ed16bd17c86c7
matifandy <matifandy@gmail.com>
2021-02-17 15:07:21 -0300

    styles

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementsByClassName("menu-icon");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
